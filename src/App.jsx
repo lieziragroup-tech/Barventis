dad442582c6081d6ab3859cc5160235be2566e56
lieziragroup-tech <lieziragroup@gmail.com>
2026-07-13 10:32:34 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -86,6 +86,12 @@ export default function App() {
           </AuthRoute>
         } />
 
+        <Route path="/register" element={
+          <AuthRoute>
+            <AuthScreen />
+          </AuthRoute>
+        } />
+
         <Route path="/" element={<RootRedirect />} />
 
         {/* SUPER ADMIN */}
